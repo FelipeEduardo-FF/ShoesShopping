d911a878ff86463ea56c538697e5c5ea3671b7ec
Felipe Eduardo <felipeeduardodealmeida@gmail.com>
2024-01-31 18:04:25 -0300

    cart

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   import './assets/main.css'
   import BaseHeader from './components/BaseHeader.vue';
   import CardList from './components/CardList.vue';
-  import Drawer from './components/Drawer.vue';
+  // import Drawer from './components/Drawer.vue';
 
 
 </script>
@@ -10,7 +10,7 @@
 <template>
   <div class="bg-white w-3/5 m-auto rounded-xl shadow-xl shadow-grey-200 mt-20">
       <BaseHeader/>
-      <Drawer/>
+      <!-- <Drawer/> -->
       <div class="p-10">
           <h2 class="text-3xl font-bold mb-8">Sneakers</h2>
           <CardList/>
--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,6 +1,15 @@
+
 <template>
-    <div class="">
+    <div class="relative flex w-full border border-slate-100 rounded-xl p-4 gap-4 mt-4">
+        <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="Sneaker"/>
 
+        <div class="flex flex-col w-full">
+            <p>tenis</p>
 
+            <div class="flex justify-between mt-5">
+                <b>R$ 12900,00 </b>
+                <img class="cursor-pointer opacity-30 hover:opacity-100 transition" src="/close.svg" >
+            </div>
+        </div>
     </div>
 </template>
